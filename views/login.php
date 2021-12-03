458ae084649cba82191fdfae88c1f88d46539501
dannyhuang <kuanhsuh@gmail.com>
2021-12-03 11:41:03 +0800

    5. Auth Controller

---
+++ views/login.php
@@ -0,0 +1,15 @@
+
+<h1>Login</h1>
+
+<form action="" method="post">
+  <div class="mb-3">
+    <label class="form-label">Email</label>
+    <input name="email" type="email" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Password</label>
+    <input name="password" type="password" class="form-control">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
---
+++ views/register.php
@@ -0,0 +1,23 @@
+
+<h1>Register</h1>
+
+<form action="" method="post">
+  <div class="mb-3">
+    <label class="form-label">Firstname</label>
+    <input name="firstname" type="text" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Lastname</label>
+    <input name="lastname" type="text" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Email</label>
+    <input name="email" type="email" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Password</label>
+    <input name="password" type="password" class="form-control">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
