26b83b6fa9ca3fd551e1fe9324fb9b91daf1334d
dannyhuang <kuanhsuh@gmail.com>
2021-12-13 11:29:01 +0800

    8. Save User

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -3,7 +3,7 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
-use app\models\RegisterModel;
+use app\models\User;
 
 class AuthController extends Controller
 {
@@ -16,20 +16,20 @@ class AuthController extends Controller
 
   public function register(Request $request)
   {
-    $registerModel = new RegisterModel();
+    $user = new User();
     if($request->isPost()) {
-      $registerModel->loadData($request->getBody());
-      if($registerModel->validate() && $registerModel->register()){
+      $user->loadData($request->getBody());
+      if($user->validate() && $user->register()){
         return 'success';
       }
 
       return $this->render('register', [
-        'model' => $registerModel
+        'model' => $user
       ]);
     }
     $this->setLayout('auth');
     return $this->render('register', [
-      'model' => $registerModel
+      'model' => $user
     ]);
   }
 }
